13860942ac56596ad22f63bd21d18d7485b3064d
SemihERCAN <113732371+SemihERCAN@users.noreply.github.com>
2022-09-28 22:30:27 +0300

    Add files via upload

---
+++ reCapDemo_Classes/src/reCapDemo_Classes/main.java
@@ -0,0 +1,11 @@
+package reCapDemo_Classes;
+
+public class main {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+		int sonuç=dortIslem.Topla(2, 5);
+		System.out.println(sonuç);
+	}
+
+}
